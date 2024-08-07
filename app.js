a588b1a6443ad91a74086bdd1b923e327a88e24a
Danik-Ux <aimdannewz@gmail.com>
2024-08-07 22:42:32 +1200

    Test with public Sentinel Hub layer

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var map = L.map('map').setView([0, 0], 2);
 
-L.tileLayer('https://services.sentinel-hub.com/ogc/wms/8f44a8b8-ccde-4121-95c8-c0735067f8de?layers=TRUE-COLOR&request=GetMap&styles=&format=image/jpeg&tilematrixset=EPSG:3857&tile={z}/{x}/{y}', {
+L.tileLayer('https://services.sentinel-hub.com/ogc/wms/06e1cf9f-14b3-4803-a4dc-fbff73f0eddf?layers=TRUE-COLOR-S2-L1C&request=GetMap&styles=&format=image/jpeg&tilematrixset=EPSG:3857&tile={z}/{x}/{y}', {
     maxZoom: 18,
     attribution: '&copy; <a href="https://www.sentinel-hub.com/">Sentinel Hub</a>'
 }).addTo(map);
